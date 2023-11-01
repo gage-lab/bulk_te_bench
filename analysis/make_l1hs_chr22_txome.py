2b54c67f43f26ac5c1402cbabf9fd54888baff14
jjjoelle <jfaybish@ucsd.edu>
2023-11-01 15:22:18 -0700

    new sims

--- analysis/make_l1hs_chr22_txome.py
+++ analysis/make_l1hs_chr22_txome.py
@@ -3,6 +3,7 @@
 __author__ = "Michael Cuoco"
 
 import os
+import random
 from collections import defaultdict
 from pathlib import Path
 
@@ -36,9 +37,9 @@ counts = defaultdict(list)
 for tx in SeqIO.parse(txome.txome_fa, "fasta"):
     counts["tx_id"].append(tx.id)
     for sample in range(0, 6):
-        if "ENS" in tx.id:
+        if "ENS" in tx.id:  # gene
             counts[sample].append(20 * len(tx.seq) // 100)
-        elif "chr" in tx.id:
+        elif "chr" in tx.id:  # rmsk
             counts[sample].append(sample * len(tx.seq) // 100)
         else:
             counts[sample].append(0)
@@ -48,3 +49,64 @@ counts = pd.DataFrame(counts).set_index("tx_id")
 txome.simulate_reads(counts, "sim_reads_1", n_jobs=32)
 
 # TODO: add additional simulations here
+
+#   SIMULATION 2: non-uniform in L1 only, constant in gene transcripts
+#   2. Simulate reads with non-uniform distributions in L1 only.
+#   Choose different number for each L1 in each sample.
+
+counts = defaultdict(list)
+for tx in SeqIO.parse(txome.txome_fa, "fasta"):  # for a transcript (tx)
+    counts["tx_id"].append(tx.id)  # add the tx id to the list
+    for sample in range(0, 6):  # for 6 samples, add more counts of the tx if it is L1HS
+        if "ENS" in tx.id:  # gene
+            counts[sample].append(20 * len(tx.seq) // 100)
+        elif "chr" in tx.id:  # rmsk
+            random_coeff = random.randint(0, 10)
+            counts[sample].append(random_coeff * len(tx.seq) // 100)
+        else:
+            counts[sample].append(0)
+
+counts = pd.DataFrame(counts).set_index("tx_id")
+
+txome.simulate_reads(counts, "sim_reads_2", n_jobs=32)
+
+
+#   SIMULATION 3: THIS IS WRONG non-uniform in gene only, constant in L1
+#   Simulate reads with non-uniform distributions in non-L1, constant L1.
+
+
+counts = defaultdict(list)
+for tx in SeqIO.parse(txome.txome_fa, "fasta"):
+    counts["tx_id"].append(tx.id)
+    for sample in range(0, 6):
+        if "ENS" in tx.id:  # gene
+            random_coeff = random.randint(0, 20)
+            counts[sample].append(random_coeff * len(tx.seq) // 100)
+        elif "chr" in tx.id:  # rmsk
+            counts[sample].append(sample * len(tx.seq) // 100)
+        else:
+            counts[sample].append(0)
+
+counts = pd.DataFrame(counts).set_index("tx_id")
+
+txome.simulate_reads(counts, "sim_reads_3", n_jobs=32)
+
+#   SIMULATION 4: non-uniform in gene only, constant in L1
+#   Simulate reads with non-uniform distributions in non-L1, constant L1.
+
+
+counts = defaultdict(list)
+for tx in SeqIO.parse(txome.txome_fa, "fasta"):
+    counts["tx_id"].append(tx.id)
+    for sample in range(0, 6):
+        if "ENS" in tx.id:  # gene
+            random_coeff = random.randint(0, 20)
+            counts[sample].append(random_coeff * len(tx.seq) // 100)
+        elif "chr" in tx.id:  # rmsk
+            counts[sample].append(10 * len(tx.seq) // 100)
+        else:
+            counts[sample].append(0)
+
+counts = pd.DataFrame(counts).set_index("tx_id")
+
+txome.simulate_reads(counts, "sim_reads_4", n_jobs=32)
