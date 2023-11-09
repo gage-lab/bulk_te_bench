9456fede1b287bf895deeb5b912a5a7c5771aaee
mikecuoco <mcuoco@ucsd.edu>
2023-11-09 12:50:40 -0800

    cleanup

--- workflow/simulate/simulate_te_counts.py
+++ workflow/simulate/simulate_te_counts.py
@@ -22,84 +22,63 @@ import pandas as pd
 import pyranges as pr
 from Bio import SeqIO
 
+# Get mean tpm in the tx count table
+counts = pd.read_csv(snakemake.input.counts, sep="\t", index_col=0)
+mean_counts = counts.mean(axis=1)
+
+tx_tpms = []
+logger.info("Iterating over fasta txs")
+for tx in SeqIO.parse(snakemake.input.txome_fa, "fasta"):
+    if tx.id in list(counts.index):
+        # Calculate tpm from counts
+        tx_tpms.append(mean_counts[tx.id] * 100 // len(tx.seq))
+
+mean_tx_tpm = np.mean(tx_tpms)
+
 # Get all l1hs loci from rmsk
 genes_gtf = pr.read_gtf(snakemake.input.genes_gtf)
 rmsk_gtf = pr.read_gtf(snakemake.input.rmsk_gtf)
 
 # Find ones that are intergenic, by seeing if there are overlaps between location of loci in rmsk and gene gtf
-df = rmsk_gtf.join(genes_gtf, how="left", report_overlap=True, suffix="_rmsk").df
-df["Intergenic"] = df["Overlap"] == (df["End"] - df["Start"])
-df["Intronic"] = df["Overlap"] < 0
-
-if df["Intergenic"].sum() < 1:
-    logger.error("The txome does not have any Intergenic loci.")
-    raise ValueError("The txome does not have any Intergenic loci.")
-if df["Intronic"].sum() < 1:
-    logger.error("The txome does not have any intronic loci.")
-    raise ValueError("The txome does not have any intronic loci.")
-
-counts_mtx = pd.read_csv(snakemake.input.counts, sep="\t")
+rmsk = rmsk_gtf.join(genes_gtf, how="left", report_overlap=True, suffix="_gene").df
+rmsk["Intronic"] = rmsk["Overlap"] == (rmsk["End"] - rmsk["Start"])
+rmsk["Intergenic"] = rmsk["Overlap"] < 0
 
 
-row_values = []
-tes = []
-logger.info("Iterating over fasta txs")
-# Get average tpm in the tx count table (inverse equation to uniform/test sims)
-for tx in SeqIO.parse(snakemake.input.txome_fa, "fasta"):
-    if tx.id in list(counts_mtx["tx_id"]):
-        # Inverse equation to uniform/test sims
-        row_values.append(
-            np.mean(
-                counts_mtx.loc[counts_mtx["tx_id"] == tx.id,].values[
-                    0
-                ][1:]
-            )
-            * 100
-            // len(tx.seq)
-        )
-    else:
-        if snakemake.wildcards.tx_sim == "test_sim":
-            if "ENS" in tx.id:
-                continue
+def contains_intergenic_intronic(df):
+    if df["Intergenic"].sum() < 1:
+        logger.error("The txome does not have any Intergenic TE loci.")
+        raise ValueError("The txome does not have any Intergenic TE loci.")
+    if df["Intronic"].sum() < 1:
+        logger.error("The txome does not have any intronic TE loci.")
+        raise ValueError("The txome does not have any intronic TE loci.")
 
-        tes.append(tx)
 
+contains_intergenic_intronic(rmsk)
 
 ## SIMULATION 1: single intergenic loci ##
 if snakemake.wildcards.te_sim == "single_intergenic_l1hs":
     logger.info("Iterating over L1HS loci")
 
-    te_counts = defaultdict(list)
-    keep_empty = False
-    # warning: this will not work for test data
-    for tx in tes:
-        te_counts["tx_id"].append(tx.id)
-        # if intergenic locus added
-        if keep_empty:
-            # Add other l1hs loci (0 counts)
-            for sample in counts_mtx.columns[1:]:
-                te_counts[sample].append(0)
+    # filter for l1hs
+    rmsk = rmsk[rmsk["gene_id"] == "L1HS"]
+    if rmsk.empty:
+        logger.error("The txome does not have any L1HS loci.")
+        raise ValueError("The txome does not have any L1HS loci.")
+    contains_intergenic_intronic(rmsk)
 
-        # if first intergenic locus
-        elif tx.id in list(
-            df.loc[
-                df["Intergenic"],
-            ]["transcript_id"]
-        ):
-            # Add row to table with 1 intergenic l1hs that has the normalized average counts for all samples
-            for sample in counts_mtx.columns[1:]:
-                te_counts[sample].append(np.mean(row_values) * len(tx.seq) // 100)
-            keep_empty = True
-
-        # if not first intergenic locus nor intergenic locus added
-        else:
-            # Add other l1hs loci (0 counts)
-            for sample in counts_mtx.columns[1:]:
-                te_counts[sample].append(0)
-
+    # for each sample, randomly choose a l1hs intergenic locus to add counts to
+    for sample in counts.columns:
+        my_te = np.random.choice(rmsk[rmsk["Intergenic"]].transcript_id)
+        for te in rmsk.itertuples():
+            if te.transcript_id == my_te:
+                counts.loc[te.transcript_id, sample] = (
+                    mean_tx_tpm * np.abs(te.End - te.Start) // 100
+                )
+            else:
+                counts.loc[te.transcript_id, sample] = 0
 else:
-    pass
+    logger.error("The simulation type is not supported.")
+    raise ValueError("The simulation type is not supported.")
 
-pd.concat([counts_mtx, pd.DataFrame(te_counts)], axis=0).to_csv(
-    snakemake.output.te_counts, sep="\t", index=False
-)
+counts.to_csv(snakemake.output.counts, sep="\t")
